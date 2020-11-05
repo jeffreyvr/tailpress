eb4606a95099b19e7c1151339ca70b1fbbadcd5d
Jeffrey van Rossum <jeffrey@vanrossum.dev>
2020-11-05 14:26:03 +0100

    Indentation.

--- single.php
+++ single.php
@@ -10,10 +10,10 @@
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
 			<?php
-				  // If comments are open or we have at least one comment, load up the comment template.
+			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
-				  comments_template();
-				  endif;
+				comments_template();
+			endif;
 			?>
 
 		<?php endwhile; ?>
