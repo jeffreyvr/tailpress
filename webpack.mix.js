a824ece86dc20d8b90ec5e9747c24a78492e7936
Jeffrey van Rossum <jeffrey@vanrossum.dev>
2021-03-11 11:38:34 +0100

    Only compile editor-styles on production builds.

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,10 @@ mix.setPublicPath(path.resolve('./'));
 mix.js('resources/js/app.js', 'js');
 
 mix.postCss("resources/css/app.css", "css");
-mix.postCss("resources/css/editor-style.css", "./");
+
+if (mix.inProduction()) {
+    mix.postCss("resources/css/editor-style.css", "./");
+}
 
 mix.options({
     postCss: [
