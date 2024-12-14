e8a07137e80031bc560c2dc9d5c839fbc790aeb5
gcordner <geoffcordner@gmail.com>
2024-12-13 20:49:38 -0800

    more fonts: sofia sans, ibm-plex-sans, open-sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ const theme = require('./theme.json');
 const tailpress = require("@jeffreyvr/tailwindcss-tailpress");
 
 /** @type {import('tailwindcss').Config} */
+// const tailpress = require('./tailpress');
+
 module.exports = {
     content: [
         './*.php',
@@ -22,19 +24,23 @@ module.exports = {
             colors: tailpress.colorMapper(tailpress.theme('settings.color.palette', theme)),
             fontSize: tailpress.fontSizeMapper(tailpress.theme('settings.typography.fontSizes', theme)),
             fontFamily: {
-                sans: ['Open Sans Condensed', 'sans-serif'], // Set Open Sans Condensed as the base sans-serif font
-            }
+                sans: ['IBM Plex Sans', 'sans-serif'],
+                'open-sans-condensed': ['"Open Sans Condensed"', 'sans-serif'],
+            },
+
         },
         screens: {
-            'xs': '480px',
-            'sm': '600px',
-            'md': '782px',
-            'lg': tailpress.theme('settings.layout.contentSize', theme),
-            'xl': tailpress.theme('settings.layout.wideSize', theme),
+            xs: '480px',
+            sm: '600px',
+            md: '782px',
+            lg: tailpress.theme('settings.layout.contentSize', theme),
+            xl: tailpress.theme('settings.layout.wideSize', theme),
             '2xl': '1440px'
         }
     },
     plugins: [
+        require('@tailwindcss/typography'),
         tailpress.tailwind
     ]
 };
+
